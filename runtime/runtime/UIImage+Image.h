6fa74aac3b38ded96b3f1c4fd9008d4b950bc18e
han <hanxiaosheng@hywingroup.com>
2019-05-13 11:09:52 +0800

    初始化项目

---
+++ runtime/runtime/UIImage+Image.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Image.h
+//  runtime
+//
+//  Created by hywin on 2019/5/10.
+//  Copyright © 2019年 SF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Image)
+
+@end
